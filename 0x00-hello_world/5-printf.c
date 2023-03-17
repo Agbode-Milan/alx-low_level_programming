8bf3e578e730924093fb0afd3c89ecb540a8179b
Agbodeka <agbodekacalebkm@gmail.com>
2023-03-17 12:39:47 -0700

    Piece of art

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - this main function is to help print a piece of art
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
