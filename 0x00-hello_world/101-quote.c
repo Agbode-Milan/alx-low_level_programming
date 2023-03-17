2b25bf4a9bfc87dd459b675d9aaec41e0840da60
Agbodeka <agbodekacalebkm@gmail.com>
2023-03-17 15:49:58 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,9 @@
 #include <unistd.h>
 
 /**
- *
- * main - A C program with unix
- *
- * Return 1: (Success)
-*/
+ * main - A c program that prints an error
+ * Return - (1) (Success)
+ */
 int main(void)
 {
 	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
