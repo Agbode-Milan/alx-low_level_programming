50fc0693b1df1a009216ae54e38dd8357584410d
Agbodeka <agbodekacalebkm@gmail.com>
2023-03-17 15:19:11 -0700

    Grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	printf("Size of a char: %1u byte(s)",sizeof(char));
+	printf("Size of a char: %1u byte(s)", sizeof(char));
 	printf("Size of an int: %1u byte(s)", sizeof(int));
 	printf("Size of a long int: %1u byte(s)", sizeof(long int));
 	printf("Size of a long long int: %1u byte(s)", sizeof(long long int));
