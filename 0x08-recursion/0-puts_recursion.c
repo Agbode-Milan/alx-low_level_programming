b1f76aecac1749c45321cbc8841306bc7cdbd958
Agbodeka <agbodekacalebkm@gmail.com>
2023-04-06 11:29:59 -0700

    A secret locked away

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,15 +1,18 @@
 #include "main.h"
+
 /**
- * _puts_recursion - function like puts();
+ * _puts_recursion - this sy=tring is printed recursively
  * @s: input
  * Return: Always 0 (Success)
  */
+
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar('\n');
+		return;
 	}
-	_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(s + 1);
 }
