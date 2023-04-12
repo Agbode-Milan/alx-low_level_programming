7cf798274ce647182c37c6c7d7310e077ada7252
Agbodeka <agbodekacalebkm@gmail.com>
2023-04-12 15:15:07 -0700

    It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stddef.h>
 #include <stdlib.h>
 
 /**
@@ -12,36 +12,37 @@
 
 char *argstostr(int ac, char **av)
 {
+	int i, n, r = 0, l = 0;
+	char *str;
+
 	if (ac == 0 || av == NULL)
-	{
 		return (NULL);
-	}
-	int len = 0;
-	for (int i = 0; i < ac; i++)
+
+	for (i = 0; i < ac; i++)
 	{
-		for (int j = 0; av[i][j] != '\0'; j++)
-		{
-			len++;
-		}
-		len++;
-	}
-	char *str = malloc(sizeof(char) * (len + 1));
-	if (str == NULL)
-	{
-		return (NULL);
-	}
-	int k = 0;
-	for (int i = 0; i < ac; i++)
-	{
-		for (int j = 0; av[i][j] != '\0'; j++)
-		{
-			str[k++] = av[i][j];
-		}
-		str[k++] = '\n';
+		for (n = 0; av[i][n] != '\0'; n++)
+			l++;
 	}
 
-	str[k] = '\0';
+	l += ac;
+
+	str = malloc(sizeof(char) * (l + 1));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (n = 0; av[i][n] != '\0'; n++)
+		{
+			str[r] = av[i][n];
+			r++;
+		}
+
+		str[r] = '\n';
+		r++;
+	}
+
+	str[r] = '\0';
 
 	return (str);
 }
-
