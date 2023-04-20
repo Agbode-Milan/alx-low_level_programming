eff1f3eb087bc14a2949c67d7a0cb3d938b1e7ca
Agbodeka <agbodekacalebkm@gmail.com>
2023-04-19 18:04:56 -0700

    To hell with circumstances; I create opportunities

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * array_iterator - iterates over an array
+ * @array: pointer to the array
+ * @size: size of the array
+ * @action: pointer to the function to perform on each element
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
+
